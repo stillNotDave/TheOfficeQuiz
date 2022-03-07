841b194155ab6612d34fa86bdbe45fb54b1311a5
stillNotDave <ujenevfqgjg@mail.ru>
2022-03-08 00:44:51 +0300

    day 2

---
+++ app/src/main/java/com/mzenin/theofficequiz/Level1.java
@@ -0,0 +1,14 @@
+package com.mzenin.theofficequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal_activity_for_levels);
+    }
+}

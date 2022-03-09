d55040c344a6e0c5c206322f30999974eff181d0
stillNotDave <ujenevfqgjg@mail.ru>
2022-03-09 20:15:08 +0300

    add java code

--- app/src/main/java/com/mzenin/theofficequiz/Level1.java
+++ app/src/main/java/com/mzenin/theofficequiz/Level1.java
@@ -2,26 +2,119 @@ package com.mzenin.theofficequiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.Random;
 
 public class Level1 extends AppCompatActivity {
+
+    Dialog dialog;
+    public int question;
+    public int answer;
+    QuestionsArray array = new QuestionsArray(); //создаем новый объект класса QuestionsArray
+    Random random = new Random();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal_activity_for_levels);
 
-
-//        //скругление углов картинки
-//        final ImageView image = (ImageView)findViewById(R.id.image1);
-//        image.setClipToOutline(true);
-//        //используя xml файл style_image_universal
+        //устанавливаем нужную цифру в уровень:
+        TextView textLevels = findViewById(R.id.textLevels);
+        textLevels.setText(R.string.level1);
 
         Window window = getWindow();
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         //Убираем верхнюю парень телефона - строку состояния
+
+        //Вызов диалогового окна
+        dialog = new Dialog(this);
+        dialog.requestWindowFeature(window.FEATURE_NO_TITLE); //убираем заголовок
+        dialog.setContentView(R.layout.preview_dialog_window);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT)); //делаем все остальное прозрачным
+        dialog.setCancelable(false); // нелья закрыть системной кнопкой
+
+        //закрытие диалогового окна
+        TextView buttonClose = dialog.findViewById(R.id.buttonClose);
+        buttonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    Intent intent = new Intent(Level1.this,GameLevels.class);
+                    startActivity(intent);
+                    finish();
+                }catch (Exception e){
+
+                }
+                dialog.dismiss(); //закрыть диалог
+            }
+        });
+
+        Button buttonContinue = dialog.findViewById(R.id.buttonContinue);
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss(); //закрыть диалог
+            }
+        });
+
+        dialog.show();
+
+        //кнопка назад
+        Button buttonBackToLevels = findViewById(R.id.buttonBackToLevels);
+        buttonBackToLevels.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try{
+                    openActivity(GameLevels.class);
+                }catch (Exception e){
+
+                }
+            }
+        });
+
+        //случайный вопрос
+        final TextView textQuestion = findViewById(R.id.line3);
+        question = random.nextInt(10);
+        textQuestion.setText(array.questions1Lvl[question]); //достаем из массива строку с номером равным случайному числу
+        //в следующих уровнях нужно будет предусмотреть, чтобы вопросы не повторялись
+
+        //варианты ответов
+        final TextView textAnswer1 = findViewById(R.id.answer1);
+        final TextView textAnswer2 = findViewById(R.id.answer2);
+        final TextView textAnswer3 = findViewById(R.id.answer3);
+        final TextView textAnswer4 = findViewById(R.id.answer4);
+
+
     }
+
+    //системная кнопка назад
+    @Override
+    public void onBackPressed(){
+        try{
+            openActivity(GameLevels.class);
+        }catch (Exception e){
+
+        }
+    }
+
+    public void openActivity(Class<?> cls){
+        Intent intent = new Intent(this, cls);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+    }
+
 }
---
+++ app/src/main/java/com/mzenin/theofficequiz/QuestionsArray.java
@@ -0,0 +1,88 @@
+package com.mzenin.theofficequiz;
+
+public class QuestionsArray {
+
+    //Массив для 1 уровня
+    final int[] questions1Lvl = {
+            R.string.question1,
+            R.string.question2,
+            R.string.question3,
+            R.string.question4,
+            R.string.question5,
+            R.string.question6,
+            R.string.question7,
+            R.string.question8,
+            R.string.question9,
+            R.string.question10
+    };
+
+    final int[] answer1Lvl1 = {
+            R.string.answer1Lvl1Correct,
+            R.string.answer1Lvl1Wrong1,
+            R.string.answer1Lvl1Wrong2,
+            R.string.answer1Lvl1Wrong3
+    };
+
+    final int[] answer2Lvl1 = {
+            R.string.answer2Lvl1Correct,
+            R.string.answer2Lvl1Wrong1,
+            R.string.answer2Lvl1Wrong2,
+            R.string.answer2Lvl1Wrong3
+    };
+
+    final int[] answer3Lvl1 = {
+            R.string.answer3Lvl1Correct,
+            R.string.answer3Lvl1Wrong1,
+            R.string.answer3Lvl1Wrong2,
+            R.string.answer3Lvl1Wrong3
+    };
+
+    final int[] answer4Lvl1 = {
+            R.string.answer4Lvl1Correct,
+            R.string.answer4Lvl1Wrong1,
+            R.string.answer4Lvl1Wrong2,
+            R.string.answer4Lvl1Wrong3
+    };
+
+    final int[] answer5Lvl1 = {
+            R.string.answer5Lvl1Correct,
+            R.string.answer5Lvl1Wrong1,
+            R.string.answer5Lvl1Wrong2,
+            R.string.answer5Lvl1Wrong3
+    };
+
+    final int[] answer6Lvl1 = {
+            R.string.answer6Lvl1Correct,
+            R.string.answer6Lvl1Wrong1,
+            R.string.answer6Lvl1Wrong2,
+            R.string.answer6Lvl1Wrong3
+    };
+
+    final int[] answer7Lvl1 = {
+            R.string.answer7Lvl1Correct,
+            R.string.answer7Lvl1Wrong1,
+            R.string.answer7Lvl1Wrong2,
+            R.string.answer7Lvl1Wrong3
+    };
+
+    final int[] answer8Lvl1 = {
+            R.string.answer8Lvl1Correct,
+            R.string.answer8Lvl1Wrong1,
+            R.string.answer8Lvl1Wrong2,
+            R.string.answer8Lvl1Wrong3
+    };
+
+    final int[] answer9Lvl1 = {
+            R.string.answer9Lvl1Correct,
+            R.string.answer9Lvl1Wrong1,
+            R.string.answer9Lvl1Wrong2,
+            R.string.answer9Lvl1Wrong3
+    };
+
+    final int[] answer10Lvl1 = {
+            R.string.answer10Lvl1Correct,
+            R.string.answer10Lvl1Wrong1,
+            R.string.answer10Lvl1Wrong2,
+            R.string.answer10Lvl1Wrong3
+    };
+}
